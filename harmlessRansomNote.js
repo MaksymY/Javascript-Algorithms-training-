2d52157da100984da8adfc912f2cdef997f36faf
MaksymY <maksym.yankivskyypro@gmail.com>
2019-10-06 16:24:12 +0200

    armlessRansomNote algorithm whith explication

---
+++ harmlessRansomNote.js
@@ -0,0 +1,25 @@
+function harmlessRansomNote(noteText, noteMagazin){
+    var magazinArray = noteMagazin.split(' ');
+    var textArray = noteText.split(' ');
+    var magazinObj = {};
+    var possibleNote = true;
+  
+    //for aeach element of array magazinArray we execute word function
+    //and we verify if element  in array magazinArray existe in magazinObj if not we initialise it at 0
+    //if it existe we add 1
+    magazinArray.forEach(word => {
+      if(!magazinObj[word]) magazinObj[word] = 0;
+      magazinObj[word]++;
+    })
+    
+    //for aeach element of array textArray we execute word function
+    //and we verify if element of textArray existe in magazinObj and if he is diffrent from 0 if this condition is okey we -1
+    textArray.forEach(word => {
+      if(magazinObj[word] > 0 && magazinObj[word] !== 0){
+        magazinObj[word]--;
+      }else possibleNote = false;
+    });
+   return possibleNote;
+  }
+  
+  harmlessRansomNote('salut ami', 'salut copain ça va copain');
